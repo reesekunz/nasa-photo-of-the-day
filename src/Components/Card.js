b2e43665eb66d1c33b1ae240541bc8f62263ec5c
Reese Kunz <reesekunz21@gmail.com>
2019-07-18 22:23:07 -0700

    changed file structure for style components

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,6 @@
 // Step 1 - Create component folder and files after laying out/designing file structure
 // Steps for creating folder/file in terminal: cd src => mkdir Components => cd Components => touch Card.js
+// FILE STRUCTURE: App contains CardView. CardView contains Card and Explanation (added below with imports). Card contains title, date, image (from api). Explanation contains explanation (from api).
 
 // Step 2 - Imports and Exports
 
@@ -16,20 +17,13 @@ function Card(props) {
   return (
     // for props.data.title (props from parameter defined in above Card function, data from useState state variable, title is the content you want to grab/display from the api (https://api.nasa.gov/planetary/apod?api_key=isAVbPeQtem6kagSm6mn7lFUGnDWamTz6wce4ykO)
     // for props.data.date (props from parameter defined in above Card function, data from useState state variable, date is the content you want to grab/display from the api (https://api.nasa.gov/planetary/apod?api_key=isAVbPeQtem6kagSm6mn7lFUGnDWamTz6wce4ykO)
-    // App contains CardView. CardView contains Card and Explanation. Card contains title, date, image (from api). Explanation contains explanation (from api).
-    // Could have also just added <p>{props.data.explanation}</p> inside <div> below and gotten rid of Explanation.js file
+    // for props.data.url (props from parameter defined in Card function, data from useState state variable, url is the content you want to grab/display from the api (https://api.nasa.gov/planetary/apod?api_key=isAVbPeQtem6kagSm6mn7lFUGnDWamTz6wce4ykO)
     <div className="Card">
       <h2>{props.data.title}</h2>
       <p>Date: {props.data.date}</p>
-
-      {props.data.url ? (
-        <img src={`${props.data.url}`} alt="need this alt to fix errors" />
-      ) : (
-        <p>Loading image of the day....</p>
-      )}
+    <img src={props.data.url} alt="alt" />
     </div>
   );
 }
 
 export default Card;
-
--- src/Components/CardView.js
+++ src/Components/CardView.js
@@ -1,12 +1,12 @@
 // Step 1 - Create component folder and files after laying out/designing file structure
 // Steps for creating folder/file in terminal: cd src => mkdir Components => cd Components => touch CardView.js
+// FILE STRUCTURE: App contains CardView. CardView contains Card and Explanation (added below with imports). Card contains title, date, image (from api). Explanation contains explanation (from api).
 
 // Step 2 - Imports and Exports
 
 // Import useState and useEffect by adding to existing React import
 import React, { useState, useEffect } from "react";
 // Import newly created component files (reference design layout/plan to see what and where you need to import components)
-// App contains CardView. CardView contains Card and Explanation (added below with imports). Card contains title, date, image (from api). Explanation contains explanation (from api).
 
 import Card from "./Card";
 import Explanation from "./Explanation";
@@ -18,6 +18,7 @@ import Axios from "axios";
 function CardView() {
   // Add useState() - Define variable as data placeholder inside CardView function
   // useState returns an array 1st element [data] = state variable. 2nd element [setData] = function to update value of state variable
+  // Need to give useState an empty string or any value so value isnt undefined (undefined would result in an error)
   const [data, setData] = useState("");
 
   // Add useEffect() - called on mount (when function is first called), takes 2 arguments - callback function and array
@@ -44,7 +45,7 @@ function CardView() {
 
   return (
     // Set props for each component (Card and Explanation) by applying them to state variable defined in UseState const [data]
-    // App contains CardView. CardView contains Card and Explanation (added below with <Card /> and <Explanation />). Card contains title, date, image (from api). Explanation contains explanation (from api).
+    // Look at file structure to look at which props to use and where. App contains CardView. CardView contains Card and Explanation (added below with <Card /> and <Explanation />). Card contains title, date, image (from api). Explanation contains explanation (from api).
 
     <div className="CardView">
       <Card data={data} />
